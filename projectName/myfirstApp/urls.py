d2d21c775c0d8bf8aed8cb2403ccca5279b0f0f6
kader01agi <md.abdul.kader.ru@gmail.com>
2023-12-05 11:08:13 +0600

    templating

--- projectName/myfirstApp/urls.py
+++ projectName/myfirstApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.apps_first_resp),
+    path('',views.apps_first_resp, name="first_app_home"),
     path('form/',views.templating)
 ]
